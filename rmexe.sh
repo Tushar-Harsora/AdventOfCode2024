adc5186ad47b3254587eaac12f3aca29745c52b1
Tushar Harsora <tushar.harsora.harsora@oracle.com>
2024-12-07 16:48:57 +0530

    Also remove debug symbols

--- rmexe.sh
+++ rmexe.sh
@@ -4,4 +4,8 @@ set -euo pipefail
 while read -r line; do
     echo "Deleting: ${line}"
     rm $line
+    if [[ -d "$line.dSYM" ]]; then
+        echo "Deleting debug symbols: $line.dSYM"
+        rm -r "$line.dSYM"
+    fi
 done < <(find . -type file | xargs -n1 -I{} file {} | grep "executable x86_64" | cut -d':' -f1)
