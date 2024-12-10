ef49e46bbbff7289dd969c0bd7167202e70855af
Tushar Harsora <tushar.harsora.harsora@oracle.com>
2024-12-10 14:30:30 +0530

    Also output debug symbols in crun.sh

--- crun.sh
+++ crun.sh
@@ -18,7 +18,7 @@ cd $latest_day_folder || { echo "Directory 'Day1' not found!"; exit 1; }
 source_file="${arg}.cpp"
 output_binary="${arg}"
 
-clang++ -std=c++20 -Xpreprocessor -fopenmp "$source_file" -o "$output_binary" -lomp $(echo ${LDFLAGS})
+clang++ -std=c++20 -g -Xpreprocessor -fopenmp "$source_file" -o "$output_binary" -lomp $(echo ${LDFLAGS})
 
 if [ $? -eq 0 ]; then
     echo "Compiled"
